8e8f307e59f69067c53f9db64d6460821661e506
kolashankar <kolashankar113@gmail.com>
2025-08-17 12:51:39 +0530

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,7 @@ const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
   webpack: (config) => {
-    config.resolve.alias['@components'] = path.resolve('./components')
-    config.resolve.alias['@lib'] = path.resolve('./lib')
-    config.resolve.alias['@hooks'] = path.resolve('./hooks')
-    config.resolve.alias['@utils'] = path.resolve('./utils')
-    config.resolve.alias['@repositories'] = path.resolve('./repositories')
-    config.resolve.alias['@app'] = path.resolve('./app')
+    config.resolve.alias['@'] = path.resolve('./')
     return config
   },
 }
